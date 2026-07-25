2c9158d36338e0f51a158361263b71e38b8920b0
G Vinay Kumar <vinay.kumar@zingoy.com>
2026-07-25 13:06:21 +0530

    feat: reduce notification auto-dismiss duration to 4 seconds and remove delivered banner programmatically

--- lib/notifier.swift
+++ lib/notifier.swift
@@ -256,8 +256,9 @@ func main() {
     NSUserNotificationCenter.default.delegate = delegate
     NSUserNotificationCenter.default.deliver(notification)
     
-    // Auto-dismiss script process after 45 seconds
-    DispatchQueue.main.asyncAfter(deadline: .now() + 45) {
+    // Auto-dismiss and remove notification after 4 seconds
+    DispatchQueue.main.asyncAfter(deadline: .now() + 4.0) {
+        NSUserNotificationCenter.default.removeDeliveredNotification(notification)
         exit(0)
     }
     
